cb902170b037723cad960675700fab7904d80e91
brokensythe <ade_1.0@live.co.uk>
2021-04-29 23:37:00 +0100

    cool extra stuff

--- index.js
+++ index.js
@@ -14,15 +14,27 @@ for (const user of users) {
     objectToString += `id: ` + user.id + `, username: ` + user.name + `, resides in: ` + user.address.city + `\n\n`
 }
 
-// listOfIds = []
+listOfIds = []
 
-// for (const user of users) {
-//     listOfIds.push(String(user.id))
-// }
+for (const user of users) {
+    listOfIds.push(user.id)
+}
 
 alert(objectToString)
 
-const currentUserId = parseInt(prompt(`Please enter your id no. 🙏`))
+// This while loop is in the wrong place, think I have to pass this over the entire script
+// while (true) {
+//     const currentUserId = parseInt(prompt(`Please enter your id no. 🙏`))
+
+//     if (listOfIds.includes(currentUserId)) {
+//         alert(`You are an approved user`)
+//         break
+//     }
+//     else {
+//         alert(`You are not an approved user`)
+//     }
+
+// }
 
 // for (const id of listOfIds) {
 //     if (currentUserId===id) {
@@ -34,6 +46,10 @@ const currentUserId = parseInt(prompt(`Please enter your id no. 🙏`))
 // }
 
 const currentUser = users[currentUserId - 1]
+
+alert(`Hello ` + currentUser.username)
+
+const toDoListToggle = confirm(`Would you like to see your to do list?`)
 
 const toDoList = []
 
@@ -48,12 +64,28 @@ let toDoString = ``
 for (const item of toDoList) {
     if (item.completed===true) {
         const syntaxUpdate = "yes"
-        toDoString += `username: ` + currentUser.username + `, todo id: ` + item.id + `, title: ` + item.title + `, completed ` + syntaxUpdate + `\n\n`
+        toDoString += `username: ` + currentUser.username + `, to do id: ` + item.id + `, title: ` + item.title + `, completed ` + syntaxUpdate + `\n\n`
     }
     else {
         const syntaxUpdate = "no"
-        toDoString += `username: ` + currentUser.username + `, todo id: ` + item.id + `, title: ` + item.title + `, completed ` + syntaxUpdate + `\n\n`
+        toDoString += `username: ` + currentUser.username + `, to do id: ` + item.id + `, title: ` + item.title + `, completed ` + syntaxUpdate + `\n\n`
     }
 }
 
-alert(toDoString)
+if (toDoListToggle===true) {
+    alert(toDoString)
+}
+
+const addANewTodo = confirm(`Do you want to add a new item to your to do list?`)
+
+// if above is true
+
+if (addANewTodo===true) {
+    const toDoInfo = prompt(`What do you need to do ?`)
+    const toDoId = todos.push({
+        userId: currentUser.id,
+        id: todos.length + 1,
+        title: toDoInfo,
+        completed: false,
+    })
+}
